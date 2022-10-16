88042740e7a69a414d9c0b5297211101c1b6a489
Soin Andrey <soinproduction@gmail.com>
2022-10-16 22:58:17 +0300

    add animation

--- source/js/functions/burger.js
+++ source/js/functions/burger.js
@@ -3,38 +3,23 @@ import { enableScroll } from '../functions/enable-scroll';
 import vars from '../_vars';
 import { elementHeight } from '../functions/customFunctions';
 
-// import { getHeaderHeight } from '../functions/header-height';
-
 import {addCustomClass} from '../functions/customFunctions'
 
-// let mobileMenu = document.querySelector('.mobile-menu');
-// let burger = document.querySelector('.site-menu');
-// let burgerClose = document.querySelector('.mobile-menu__close');
+let mobileMenu = document.querySelector('.mobile-menu');
+let burger = [...document.querySelectorAll('.burger')];
+const mobileMenuHandler = function( menu, button, body) {
+  button.addEventListener('click', function(){
+    elementHeight(vars.menu, "menu-height");
+    menu.classList.toggle('active');
+    button.classList.toggle('active');
+    body.classList.toggle('fixed-body');
+  })
+}
 
-// const mobileMenuHandler = function( menu, button) {
-//   button.addEventListener('click', function(){
-//     elementHeight(vars.menu, "menu-height");
-//     menu.classList.toggle('active');
-//     button.classList.toggle('active');
-//     disableScroll();
+burger.map(function(btn){
+  mobileMenuHandler(mobileMenu,btn, vars.bodyEl);
+})
 
-//   })
-// }
-
-// const hideMenuHandler = function(menu, button) {
-//     menu.classList.remove('active');
-//     button.classList.remove('active');
-//     enableScroll()
-// }
-
-// mobileMenuHandler(mobileMenu,burger);
-// burgerClose.addEventListener('click', function(e){
-//   hideMenuHandler(mobileMenu,burger);
-// });
-
-// document.querySelector('.fixed-button__close').addEventListener('click', function(){
-//   document.querySelector('.fixed-button').classList.add('hide')
-// })
 
 
 
